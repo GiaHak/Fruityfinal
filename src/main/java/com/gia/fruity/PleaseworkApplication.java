01eaf14a3b7e60cd307beeb0cddca1b4a27ad61b
GiaHak <giahak@yahoo.com>
2021-10-04 23:05:57 -0400

    initial

---
+++ src/main/java/com/gia/fruity/PleaseworkApplication.java
@@ -0,0 +1,13 @@
+package com.gia.fruity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PleaseworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PleaseworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gia/fruity/PleaseworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gia.fruity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PleaseworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
